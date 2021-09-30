79599c276d7658d386a2d35886243e91a891a8ad
Garrett Broberg <GBroberg@foundationsoft.com>
2021-09-30 19:18:07 -0400

    Added http client to get data

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace astro
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly string ISS_API_URL = "http://api.open-notify.org/astros.json";
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetStringAsync(ISS_API_URL);
+                System.Console.WriteLine(response.ToString());
+            }
         }
     }
 }
